2caef430d3891d08bfea26592de759adb5a4c9cf
Rendy-ptr <rendyp348@gmail.com>
2025-11-13 13:37:33 +0700

    feat(railway/init-app.sh): create init script to run database migrations, clear caches, and cache routes and views for Laravel application

---
+++ railway/init-app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+php artisan migrate --force
+
+php artisan optimize:clear
+
+php artisan config:cache
+
+php artisan event:cache
+
+php artisan route:cache
+
+php artisan view:cache
+
+echo "Application initialized successfully."
